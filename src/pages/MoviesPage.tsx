cf3b98eb61f78493120f2dab5c3c47ab763d6173
regvud <silveron.regvud@gmail.com>
2023-09-12 02:19:17 +0300

    PAGINATION Backup

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -6,32 +6,30 @@ import {MovieList} from "../components/Movies";
 import {movieActions} from "../redux/slices/movieSlice";
 
 const MoviesPage: FC = () => {
-        const dispatch = useAppDispatch();
-        const [params, setParams] = useSearchParams({page: '1'});
-        const {movieData} = useAppSelector(state => state.movies);
-        const currentPage = params.get('page')
+    const dispatch = useAppDispatch();
+    const [params, setParams] = useSearchParams({page: '1'});
+    const {movieData} = useAppSelector(state => state.movies);
+    const currentPage = params.get('page')
 
-        useEffect(() => {
-            dispatch(movieActions.getAllMovies(+currentPage))
-        }, [currentPage]);
+    useEffect(() => {
+        dispatch(movieActions.getAllMovies(+currentPage))
+    }, [currentPage]);
 
 
-        return (
-            <>
-                <div>
-                    <button
-                        disabled={+params.get('page') <= 1}
-                        onClick={() => setParams({page: (+currentPage - 1).toString()})}>prev
-                    </button>
-                    <button
-                        disabled={+params.get('page') >= 500}
-                        onClick={() => setParams({page: (+currentPage + 1).toString()})}>next
-                    </button>
-                </div>
-                <MovieList movieData={movieData}/>
-            </>
-        );
-    }
-;
-
+    return (
+        <>
+            <div>
+                <button
+                    disabled={+params.get('page') <= 1}
+                    onClick={() => setParams({page: (+currentPage - 1).toString()})}>prev
+                </button>
+                <button
+                    disabled={+params.get('page') >= 500}
+                    onClick={() => setParams({page: (+currentPage + 1).toString()})}>next
+                </button>
+            </div>
+            <MovieList movieData={movieData}/>
+        </>
+    );
+}
 export {MoviesPage};
