f464d350b7be7f8ea1e2985c324883c246cc4707
regvud <silveron.regvud@gmail.com>
2023-09-12 00:31:14 +0300

    pagination issue

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -9,30 +9,38 @@ const MoviesPage = () => {
     const dispatch = useAppDispatch();
     const [params, setParams] = useSearchParams({page: '1'});
     const {movieData} = useAppSelector(state => state.movies);
-    // const currentPage = params.get('page')
+    const currentPage = params.get('page')
 
     useEffect(() => {
         dispatch(movieActions.getAllMovies(+params.get('page')))
-    }, [params.get('page')]);
+    }, [currentPage]);
 
-    const handlePage = (action: '-' | '+'): void => {
-        if (action === '+') {
-            setParams(prev => {
-                prev.set('page', (parseInt(params.get('page')) + 1).toString())
-                return prev
-            })
+    // const handlePage = (action: '-' | '+'): void => {
+    //     if (action === '+') {
+    //         setParams(prev => {
+    //             prev.set('page', (parseInt(params.get('page')) + 1).toString())
+    //             return prev
+    //         })
+    //
+    //     } else {
+    //         // const decrementedPage = (parseInt(currentPage) - 1).toString()
+    //         // setParams({page: decrementedPage})
+    //         setParams(prev => {
+    //             prev.set('page', (parseInt(params.get('page')) - 1).toString())
+    //             return prev
+    //         })
+    //     }
+    //
+    // }
 
-        } else {
-            // const decrementedPage = (parseInt(currentPage) - 1).toString()
-            // setParams({page: decrementedPage})
-            setParams(prev => {
-                prev.set('page', (parseInt(params.get('page')) - 1).toString())
-                return prev
-            })
-        }
+    const handlePage = (): void => {
+
+        setParams(prev => {
+            prev.set('page', (parseInt(params.get('page')) + 1).toString())
+            return prev
+        })
 
     }
-
 
     console.log(movieData);
 
@@ -41,11 +49,11 @@ const MoviesPage = () => {
             <div>
                 <button
                     disabled={+params.get('page') <= 1}
-                    onClick={() => handlePage('-')}>prev
+                    onClick={handlePage}>prev
                 </button>
                 <button
                     disabled={+params.get('page') >= 500}
-                    onClick={() => handlePage('+')}>next
+                    onClick={handlePage}>next
                 </button>
             </div>
             <MovieList movieData={movieData}/>
