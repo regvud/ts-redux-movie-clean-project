1a97d20e31241607dd8e8df81bd57452fd14d42b
regvud <silveron.regvud@gmail.com>
2023-12-04 23:35:52 +0200

    cleaned

--- src/components/HOC/Header/Header.tsx
+++ src/components/HOC/Header/Header.tsx
@@ -2,29 +2,30 @@ import { NavLink } from "react-router-dom";
 
 import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 import { movieActions } from "../../../redux/slices/movieSlice";
-import icon from '../../../assets/istockphoto-879131204-612x612.jpg'
-import css from './header.module.css'
-import { useState } from "react";
+import icon from "../../../assets/istockphoto-879131204-612x612.jpg";
+import css from "./header.module.css";
 
 const Header = () => {
-    const theme = useAppSelector(state => state.movies.theme);
-    const dispatch = useAppDispatch();
+  const theme = useAppSelector((state) => state.movies.theme);
+  const dispatch = useAppDispatch();
 
-    const changeTheme = () => {
-        dispatch(movieActions.themeSwitch(!theme))
-    };
+  const changeTheme = () => {
+    dispatch(movieActions.themeSwitch(!theme));
+  };
 
-    return (
-        <div className={theme ? css.dark : css.light}>
-            <button className={css.button} onClick={changeTheme}>{theme ? 'Dark Mode' : 'Light Mode'}</button>
-            <NavLink to={'movies'}>Movies</NavLink>
-            <NavLink to={'genres'}>Genres</NavLink>
-            <div className={css.icon}>
-                <img src={icon} alt="user-icon" />
-                <span>username</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className={theme ? css.dark : css.light}>
+      <button className={css.button} onClick={changeTheme}>
+        {theme ? "Dark Mode" : "Light Mode"}
+      </button>
+      <NavLink to={"movies"}>Movies</NavLink>
+      <NavLink to={"genres"}>Genres</NavLink>
+      <div className={css.icon}>
+        <img src={icon} alt="user-icon" />
+        <span>username</span>
+      </div>
+    </div>
+  );
 };
 
 export { Header };
